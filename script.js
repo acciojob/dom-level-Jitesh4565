15df0691797ce0fba15bc895dd2705ec7b1570ee
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-24 17:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function finder(){
+  let element=document.getElementById("level");
+  let level=0;
+  while(element.parentElement)
+	  {
+		  level++;
+		  element=element.parentElement;
+	  }
+	return level;
+}
+const ans=finder();
+console.log(`The level of the element is:${ans}`);
